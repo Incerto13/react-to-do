0e322df4863ec12c92fb0d804e304a0ac80b0612
Hezekiah <hezekiah@hezekiahs-mbp.myfiosgateway.com>
2024-07-07 12:53:49 -0400

    test

--- run-docker.sh
+++ run-docker.sh
@@ -4,5 +4,5 @@ source .env
 echo REACT_APP_TO_DO_DOMAIN_SERVER=$REACT_APP_TO_DO_DOMAIN_SERVER > web/.env
 echo '****************************** debug web/.env *********************'
 cat web/.env
-echo '****************************** debug web/.env *********************'
+echo '****************************** debug web/.env **********************'
 sudo docker-compose up --build -d
