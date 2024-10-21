3ce0e7053a905f7210736cd6c9c150b2c463575f
incerto13 <hezekiah@hezekiahs-mbp.myfiosgateway.com>
2024-10-21 09:44:08 -0400

    update docker-compose and script

--- run-docker.sh
+++ run-docker.sh
@@ -1,5 +1,6 @@
 # !/bin/bash
 
 source .env
-echo REACT_APP_TO_DO_DOMAIN_SERVER=$REACT_APP_TO_DO_DOMAIN_SERVER > web/.env
+printf "REACT_APP_TO_DO_DOMAIN_SERVER=$REACT_APP_TO_DO_DOMAIN_SERVER\nTYPEORM_HOST=react-to-do_postgres" > web/.env
+echo "successfully created web/.env file"
 sudo docker compose up --build -d
