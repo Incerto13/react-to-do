d6b22cb1d34e26d9a7e2674dc13195be703a7e23
incerto13 <hezekiah@Hezekiahs-MacBook-Pro.local>
2024-10-24 17:42:23 -0400

    update build pipeline

---
+++ build-docker.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+
+# generate .env file in web directory (need to do this when pushing image to docker hub bc React loads env at build time)
+source ../.env
+printf "REACT_APP_TO_DO_SERVER_URL=$REACT_APP_TO_DO_SERVER_URL" > web/.env
+echo "successfully created web/.env file"
+# build for github workflow that pushes images to docker hub
+sudo docker compose build
