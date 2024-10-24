4a66902bb2706f805b9b9d747124abe02c9b846d
incerto13 <hezekiah@Hezekiahs-MacBook-Pro.local>
2024-10-24 17:47:16 -0400

    fix .env path

--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 
 # generate .env file in web directory (need to do this when pushing image to docker hub bc React loads env at build time)
-source ../.env
+source .env # .env file will be in root directory (github workflow)
 printf "REACT_APP_TO_DO_SERVER_URL=$REACT_APP_TO_DO_SERVER_URL" > web/.env
 echo "successfully created web/.env file"
 # build for github workflow that pushes images to docker hub
